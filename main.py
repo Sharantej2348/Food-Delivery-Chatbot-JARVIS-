bccbba36add2203d91e24475dc96e0e58b9ba4fb
Sharantej2348 <sharantej7623@gmail.com>
2025-07-26 00:45:05 +0530

    Updated menu names

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def track_order(parameters: dict, session_id: str):
     order_id = int(parameters['order_id']) 
     order_status = db_helper.get_order_status(order_id)
     if order_status:
-        fulfillment_tKext = f"The order status for order id: {order_id} is: {order_status}"
+        fulfillment_text = f"The order status for order id: {order_id} is: {order_status}"
     else:
         fulfillment_text = f"No order found with order id: {order_id}"
 
